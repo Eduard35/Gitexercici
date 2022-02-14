17d4145ff867e420a77bac82d7c8c4ec6733d112
Eduard <eduardandreizahorneanu@iesmontsia.org>
2022-02-14 09:27:45 +0100

    prova

--- arraygit.java
+++ arraygit.java
@@ -33,4 +33,10 @@ public class arrays {
         }
 
     }
+
+    class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+        }
+    }
 }
